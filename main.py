8c102f715601d51361bd5cbabf7b5204e82f3375
mburuxx <njihiamburu.3@gmail.com>
2025-01-23 05:44:13 +0300

    Cleaning the data

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # Build streamlit UI
 import streamlit as st
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content
+)
 
 st.title("Scrapezilla")
 
 url = st.text_input("Enter a Website's URL")
 
 if st.button("Scrape Site"):
-    st.write("Scraping the website....")
+    st.write("Scraping the website")
+
+    results = scrape_website(url)
+    body_content = extract_body_content(results)
+    cleaned_content = clean_body_content(body_content)
+   
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
--- scrape.py
+++ scrape.py
@@ -1,24 +1,57 @@
 # Application's logic using selenium
 
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+from bs4 import BeautifulSoup
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+
+AUTH = 'brd-customer-hl_aa426599-zone-scrapezilla:5gp3bo7nebl4'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 
 def scrape_website(website):
-    print("Launching data ...")
+    print('Connecting to Scraping Browser...')
 
-    chrome_driver_path = "./chromedriver"
-
-    options = webdriver.ChromeOptions()
-
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
         driver.get(website)
-        print("Page loaded")
+        print("Waiting captcha to solve...")
+        solve_res = driver.execute(
+            "executeCdpCommand",
+            {
+                "cmd": "Captcha.waitForSolve",
+                "params": {"detectTimeout": 10000},
+            },
+        )
+        print("Captcha solve status:", solve_res["value"]["status"])
+        # print('Taking page screenshot to file page.png')
+        # driver.get_screenshot_as_file('./page.png')
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        time.sleep(10)
-
         return html
-    finally: 
-        driver.quit()
+    
+# Clean content
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    # Get text
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
